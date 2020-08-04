1468494e8061ab4e9731ff60d6bf523eb8caf63e
Sharetta Martin <shaimrt@gmail.com>
2020-08-04 15:58:36 +0000

    Working models and new database

--- myapp/models.py
+++ myapp/models.py
@@ -1,19 +1,26 @@
 from django.db import models
-#from localflavor.us.models import USStateField, USZipCodeField
+from users.models import User
+
+# from localflavor import USStateField, USZipCodeField
 
 
-#class Patient(models.Model):
-    
-    #name = models.CharField(max_length=100),
-    #birthday = models.DateField(null=True, blank=True),
-    #city = models.CharField(max_length=255),
-    #state = USStateField(null=True, blank=True)                       
-    #zip_code = USZipCodeField(null=True, blank=True)
-    #avatar = models.ImageField(upload_to='static/images', null=True, blank=True)
-    #blood_type = models.CharField(max_length=100)
-    
+class Patient(models.Model):
+    name = models.CharField(max_length=100)
+    birthday = models.DateField(null=True, blank=True)
+    city = models.CharField(max_length=255)
+    # state = USStateField(null=True, blank=True)                  
+    # zip_code = USZipCodeField(null=True, blank=True)
+    avatar = models.ImageField(upload_to='static/images', null=True, blank=True)
+    blood_type = models.CharField(max_length=100)
+    hospital = models.CharField(max_length=100)
+    need = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 class Donor(models.Model):
-  name = models.CharField(max_length=100),
-  birthday = models.DateField(null=True, blank=True),
-  blood_type = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    birthday = models.DateField(null=True, blank=True)
+    blood_type = models.CharField(max_length=100)
+    donation_type = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
